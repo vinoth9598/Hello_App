5865a4d03482c44f91f8a1bd797f22ade0bf0408
vinoth9598 <pvinoth9598@gmail.com>
2026-03-14 12:35:14 +0530

    feature/UC3-display-name-default

---
+++ HelloApp_UC3.java
@@ -0,0 +1,11 @@
+public class HelloApp_UC3 {
+    public static void main(String [] args){
+
+        String name = "World";
+        if(args.length > 0){
+            name = args[0];
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
